cf2f71207dd7eb3576ef056bbdd180c4c64e4716
Kavindu <gayanthakavindu3@gmail.com>
2024-06-12 00:07:25 +0530

    calculation work change

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,40 +1,56 @@
-let number1 = 0;
-let number2 = 0;
-let symbol = null;
-
+let numbers = [];
+let operators = [];
 
 $('.number').click(function () {
     appendToDisplay($(this).text());
-})
+});
 
 function appendToDisplay(value) {
     $('#display-input').val($('#display-input').val() + value);
 }
 
-
-
 $('.symbol').click(function () {
-    number1 = $('#display-input').val();
-    $('#display-input').val("");
-    symbol = $(this).text();
-})
-
-$('#btn-c').on('click', () => {
-    $('#display-input').val("")
-})
-
-$('.equal').click(function () {
-    number2 = $('#display-input').val();
-    if (symbol === '+') {
-        $('#display-input').val((+number1) + (+number2));
-    } else if (symbol === '-') {
-        $('#display-input').val((+number1) - (+number2));
-    } else if (symbol === '*') {
-        $('#display-input').val((+number1) * (+number2));
-    } else if (symbol === '/') {
-        $('#display-input').val((+number1) / (+number2));
-    }
+    let currentValue = $('#display-input').val();
+    numbers.push(currentValue);
+    operators.push($(this).text());
+    $('#sym').val($(this).text())
+    $('#display-input').val(""); // Clear display for next number input
 });
 
+$('#btn-c').on('click', () => {
+    $('#display-input').val("");
+    $('#sym').val("")
+    numbers = [];
+    operators = [];
+});
 
+$('.equal').click(function () {
+    let currentValue = $('#display-input').val();
+    numbers.push(currentValue);
 
+    // Perform calculations in sequence
+    let result = parseFloat(numbers[0]);
+    for (let i = 0; i < operators.length; i++) {
+        let nextNumber = parseFloat(numbers[i + 1]);
+        if (operators[i] === '+') {
+            result += nextNumber;
+        } else if (operators[i] === '-') {
+            result -= nextNumber;
+        } else if (operators[i] === '*') {
+            result *= nextNumber;
+        } else if (operators[i] === '/') {
+            result /= nextNumber;
+        }
+    }
+
+    $('#display-input').val(result);
+    numbers = [];
+    operators = [];
+});
+
+$('#btn-x').click(function () {
+    let currentValue = $('#display-input').val();
+    if (currentValue.length > 0) {
+        $('#display-input').val(currentValue.slice(0, -1));
+    }
+});
